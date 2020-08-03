0957c705342f411fa1f4a56cee996c98634495fe
Saw <deesaw@deloitte.com>
2020-08-03 15:02:13 +0530

    first commit

--- tmt_main.py
+++ tmt_main.py
@@ -37,7 +37,7 @@ def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
+            #flash('No file part')
             return redirect(request.url)
         file = request.files['file']
         print(file)
